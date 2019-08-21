57eb19d71d568cd09b3e7d71eab9e1b94d2fa62c
kellyrombough <kellyrombough270@gmail.com>
2019-08-21 00:03:23 -0400

    Adding first edge detection method, which simply compares adjacent pixels

--- src/Edges/EdgeDetection.java
+++ src/Edges/EdgeDetection.java
@@ -8,36 +8,11 @@ import java.awt.Color;
 
 public class EdgeDetection {
     public static void main (String[] args) {
-        BufferedImage img = null;
-        File f = null;
-
-        try{
-          f = new File("src/Edges/skyline.jpg");
-          img = ImageIO.read(f);
-          int width = img.getWidth();
-          int height = img.getHeight();
-
-          for (int i = 0; i<width-1; i++) {
-              for (int j=0; j<height-1; j++) {
-                  Color color1 = new Color(img.getRGB(i, j));
-                  Color color2 = new Color(img.getRGB(i+1, j));
-                  Color color3 = new Color(img.getRGB(i, j+1));
-                  int black=new Color(0,0,0).getRGB();
-                  int white = new Color(255, 255, 255).getRGB();
-                  int diff1 =  Math.abs(color1.getBlue() + color1.getGreen() + color1.getRed() + color1.getAlpha()
-                          - color2.getGreen() -color2.getBlue() - color2.getRed() - color2.getAlpha());
-                  int diff2 =  Math.abs(color1.getBlue() + color1.getGreen() + color1.getRed() + color1.getAlpha()
-                          - color3.getGreen() -color3.getBlue() - color3.getRed() - color3.getAlpha());
-                  if (diff1 > 30 && diff2 > 30) img.setRGB(i, j, white);
-                  else img.setRGB(i, j, black);
-              }
-          }
-          ImageIO.write(img, "jpg", new File("image10.jpg"));
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Wrong number of args.");
         }
-
-        catch(IOException e){
-            System.out.println(e);
-        }
-        SimpleEdgeDetection.run();
+        String filename = args[0];
+        int limit = Integer.parseInt(args[1]);
+        SimpleEdgeDetection.run(filename, limit);
     }
 }
--- src/Edges/SimpleEdgeDetection.java
+++ src/Edges/SimpleEdgeDetection.java
@@ -1,7 +1,43 @@
 package Edges;
+
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.awt.Color;
+
 public class SimpleEdgeDetection {
-    public static void run() {
-        System.out.println("Hello");
+    public static void run(String filename, int limit) {
+        BufferedImage img = null;
+        File f = null;
+
+        try{
+            f = new File(filename);
+            img = ImageIO.read(f);
+            int width = img.getWidth();
+            int height = img.getHeight();
+
+            for (int i = 0; i<width-1; i++) {
+                for (int j=0; j<height-1; j++) {
+                    Color color1 = new Color(img.getRGB(i, j));
+                    Color color2 = new Color(img.getRGB(i+1, j));
+                    Color color3 = new Color(img.getRGB(i, j+1));
+                    int black=new Color(0,0,0).getRGB();
+                    int white = new Color(255, 255, 255).getRGB();
+                    int diff1 =  Math.abs(color1.getBlue() + color1.getGreen() + color1.getRed() + color1.getAlpha()
+                            - color2.getGreen() -color2.getBlue() - color2.getRed() - color2.getAlpha());
+                    int diff2 =  Math.abs(color1.getBlue() + color1.getGreen() + color1.getRed() + color1.getAlpha()
+                            - color3.getGreen() -color3.getBlue() - color3.getRed() - color3.getAlpha());
+                    if (diff1 > limit && diff2 > limit) img.setRGB(i, j, white);
+                    else img.setRGB(i, j, black);
+                }
+            }
+            ImageIO.write(img, "jpg", new File("output.jpg"));
+        }
+
+        catch(IOException e){
+            System.out.println(e);
+        }
     }
 }
 
